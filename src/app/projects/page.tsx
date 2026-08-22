a4ae71288aec75104c48a74ae44c2d04fa170c72
Royakash2 <yahmeakash@gmail.com>
2026-08-22 20:07:20 +0600

    new page created for show all project

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Projects from "@/components/sections/projects";
+
+export default function ProjectsPage() {
+  return (
+    <div className="pt-24 pb-16 min-h-screen">
+      <div className="mx-auto max-w-5xl px-6">
+        <Projects />
+      </div>
+    </div>
+  );
+}
