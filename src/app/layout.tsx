14a067cbb167852693e2c55a1ce35009b03fba04
Royakash2 <yahmeakash@gmail.com>
2026-08-30 20:02:28 +0600

    change the file name floating nav to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import FloatingNav from "@/components/floating-nav";
+import FloatingNav from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({
